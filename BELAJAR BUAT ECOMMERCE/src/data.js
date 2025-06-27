2e3453adef1b6d05cde030c5715b7b000c3da9f4
Teuku Neo <155743925+TeukuNeo@users.noreply.github.com>
2025-06-27 13:56:03 +0700

    Add files via upload

---
+++ BELAJAR BUAT ECOMMERCE/src/data.js
@@ -0,0 +1,27 @@
+
+// array is used for stored the data of items
+let shopItemsData = [{
+    id: "CS",
+    name: "Casual Shirt",
+    price: 45,
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing",
+    img: "images/img-1.jpg"
+}, {
+    id: "OS",
+    name: "Office Shirt",
+    price: 100,
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing",
+    img: "images/img-2.jpg"
+}, {
+    id: "TS",
+    name: "T Shirt",
+    price: 25,
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing",
+    img: "images/img-3.jpg"
+}, {
+    id: "MS",
+    name: "Mens Suit",
+    price: 300,
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing",
+    img: "images/img-4.jpg"
+}]
